5a121bf6539f93318f799451d3ae6db55ed7c31a
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2022-12-09 23:36:25 -0300

    Spring: Buscar por ID

--- crud-spring/src/main/java/com/loiane/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/loiane/crudspring/controller/CourseController.java
@@ -1,11 +1,13 @@
 package com.loiane.crudspring.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,13 +31,20 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable long id) {
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody Course course){
+    public Course create(@RequestBody Course course) {
         // System.out.println(course.getName());
         // return ResponseEntity.status(HttpStatus.CREATED)
-        //     .body(courseRepository.save(course));
+        // .body(courseRepository.save(course));
         return courseRepository.save(course);
     }
-    
+
 }
