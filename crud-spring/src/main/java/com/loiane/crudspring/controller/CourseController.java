08eba8604ca750c9a8a9b6458f6fef9ad4eb18f3
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2022-12-11 18:53:37 -0300

    Atualizar Curso: API Spring HTTP PUT

--- crud-spring/src/main/java/com/loiane/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/loiane/crudspring/controller/CourseController.java
@@ -1,14 +1,12 @@
 package com.loiane.crudspring.controller;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -35,6 +33,18 @@ public class CourseController {
     public ResponseEntity<Course> findById(@PathVariable long id) {
         return courseRepository.findById(id)
                 .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable long id, @RequestBody Course couse) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(couse.getName());
+                    recordFound.setCategory(couse.getCategory());
+                    Course updated = courseRepository.save(recordFound);
+                    return ResponseEntity.ok().body(updated);
+                })
                 .orElse(ResponseEntity.notFound().build());
     }
 
