6437124b7f2c0c900ed80e83fa5f763718437863
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2024-11-24 17:15:35 -0300

    Persistência de Enums no JPA (parte 2)

--- crud-spring/src/main/java/com/loiane/crudspring/dto/mapper/CourseMapper.java
+++ crud-spring/src/main/java/com/loiane/crudspring/dto/mapper/CourseMapper.java
@@ -3,33 +3,45 @@ package com.loiane.crudspring.dto.mapper;
 import org.springframework.stereotype.Component;
 
 import com.loiane.crudspring.dto.CourseDTO;
+import com.loiane.crudspring.enums.Category;
 import com.loiane.crudspring.model.Course;
 
 @Component
 public class CourseMapper {
 
-    public CourseDTO toDTO(Course course){
+    public CourseDTO toDTO(Course course) {
 
-        if (course == null){
+        if (course == null) {
             return null;
         }
-        
-        return new CourseDTO(course.getId(), course.getName(), course.getCategory());
+
+        return new CourseDTO(course.getId(), course.getName(), course.getCategory().getValue());
     }
 
-    public Course toEntity(CourseDTO courseDTO){
+    public Course toEntity(CourseDTO courseDTO) {
 
-        if (courseDTO == null){
+        if (courseDTO == null) {
             return null;
         }
 
         Course course = new Course();
-        if(courseDTO.id() != null) {
+        if (courseDTO.id() != null) {
             course.setId(courseDTO.id());
         }
         course.setName(courseDTO.name());
-        course.setCategory(courseDTO.category());
-        course.setStatus("Ativo");
+        course.setCategory(convertCategoryValue(courseDTO.category()));
         return course;
-    }   
+    }
+
+    public Category convertCategoryValue(String value) {
+        if(value == null) {
+            return null;
+        }
+
+        return switch(value) {
+            case "Front-end" -> Category.FRONT_END;
+            case "Back-end" -> Category.BACK_END;
+            default -> throw new IllegalArgumentException("Categoria inválida: " + value);
+        };
+    }
 }
