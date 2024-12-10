46ca23d563cfbd441c5938b485f31d63c8f3d280
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2024-12-09 21:37:45 -0300

    57 validando enums com java bean validations

--- crud-spring/src/main/java/com/loiane/crudspring/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/loiane/crudspring/dto/CourseDTO.java
@@ -1,6 +1,8 @@
 package com.loiane.crudspring.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.loiane.crudspring.enums.Category;
+import com.loiane.crudspring.enums.validation.ValueOfEnum;
 import com.loiane.crudspring.model.Lesson;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -15,6 +17,6 @@ import java.util.List;
 public record CourseDTO(
         @JsonProperty("_id") Long id,
         @NotBlank @NotNull @Length(min = 5, max = 100) String name,
-        @NotNull @Length(max = 10)  @Pattern(regexp = "Back-end|Front-end") String category,
+        @NotNull @Length(max = 10)  @ValueOfEnum(enumClass = Category.class) String category,
         @NotNull @NotEmpty @Valid List<LessonDTO> lessons){
 }
